9a7138d93b1bbe94a78950b9cd5f08fba35381fd
Val Huber <valjhuber@gmail.com>
2022-10-04 18:51:45 -0700

    config info

--- run.py
+++ run.py
@@ -6,7 +6,9 @@ from sqlalchemy import CHAR, Column, DateTime, Float, ForeignKey, Index, Integer
 
 # db: docker run --name sqlsvr-container --net dev-network -p 1433:1433 -d apilogicserver/sqlsvr-m1:version1.0.0
 
-engine = create_engine("mssql+pyodbc://SA:MyPass@word@sql:1433/NORTHWND?driver=ODBC+Driver+18+for+SQL+Server&trusted_connection=no&Encrypt=no")
+engine = create_engine("mssql+pyodbc://sa:MyPass@word@localhost:1433/NORTHWND?driver=ODBC+Driver+18+for+SQL+Server&trusted_connection=no&Encrypt=no")
+# times out: engine = create_engine("mssql+pyodbc://SA:MyPass@word@sqlsvr-container:1433/NORTHWND?driver=ODBC+Driver+18+for+SQL+Server&trusted_connection=no&Encrypt=no")
+# times out: engine = create_engine("mssql+pyodbc://SA:MyPass@word@sqlsvr-container:1433/NORTHWND?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes")
 
 metadata = MetaData(engine)
 metadata.reflect(engine)
